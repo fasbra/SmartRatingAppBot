ab5ed0c56b31423d5ff4e187774e50bc75e3b500
julianogiampaoli <julianogiampaoli@gmail.com>
2014-09-13 11:56:49 -0300

    Commit inicial

---
+++ SmartRatingApp/Class/Bots/PontoFrio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartRatingApp.Class.Bots
+{
+    public class PontoFrio : IBotConcorrente
+    {
+        public IEnumerable<LeituraProduto> RealizarPesquisa(IEnumerable<LeituraProduto> requisicoes)
+        {
+            //Codigo que raspa o site do PontoFrio usando o HAP
+            return null;
+        }
+    }
+}
---
+++ SmartRatingApp/Class/RequisicaoLeituraVO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartRatingApp.Class
+{
+    public class LeituraProduto
+    {
+        public int IdProduto { get; set; }
+        public int IdConcorrente { get; set; }
+        public string NomeProduto { get; set; }
+        public string Guid { get; set; }
+    }
+}
