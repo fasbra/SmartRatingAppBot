c5607f286ac4bc493b7cc0377a381092d3bf0edd
julianogiampaoli <julianogiampaoli@gmail.com>
2014-09-21 17:35:58 -0300

    Raspagem do ponto frio

---
+++ SmartRatingApp/Class/IBotConcorrente.cs
@@ -0,0 +1,14 @@
+﻿using SmartRatingApp.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartRatingApp
+{
+    public interface IBotConcorrente
+    {
+        IEnumerable<LeituraProduto> RealizarPesquisa(IEnumerable<LeituraProduto> requisicoes);
+    }
+}
